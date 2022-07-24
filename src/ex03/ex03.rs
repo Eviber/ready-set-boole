e114369bd3e4fc2ecc4a0a67b4d7229842ee33d7
Youva GAUDE <ygaude@student.42.fr>
2022-07-24 21:40:11 +0200

    big rework, can probably be better

--- src/ex03/ex03.rs
+++ src/ex03/ex03.rs
@@ -1,6 +1,5 @@
 // an AST to parse logical expressions in rpn
 
-#[allow(dead_code)]
 #[derive(Debug, Clone, Copy)]
 enum BinOp {
     And,
@@ -17,95 +16,132 @@ enum Node {
     Val(bool),
 }
 
+// error type
+enum ParseError {
+    MissingOperand,
+    InvalidCharacter(char),
+    UnbalancedExpression,
+}
+
 use Node::*;
 use BinOp::*;
+use ParseError::*;
+use std::fmt;
 
-fn parse(input: &str) -> Result<Node, String> {
-    let mut stack = Vec::with_capacity(42);
+impl TryFrom<char> for BinOp {
+    type Error = ParseError;
 
-    for c in input.chars() {
+    fn try_from(c: char) -> Result<Self, Self::Error> {
         match c {
-            '0' => stack.push(Val(false)),
-            '1' => stack.push(Val(true)),
-            '!' => {
-                let operand = stack.pop().unwrap();
-                stack.push(Not { operand: Box::new(operand) });
-            },
-            '&' | '|' | '^' | '>' | '=' => {
-                let right = stack.pop().unwrap();
-                let left = stack.pop().unwrap();
-                stack.push(Binary {
-                    op: match c {
-                        '&' => And,
-                        '|' => Or,
-                        '^' => Xor,
-                        '>' => Impl,
-                        '=' => Leq,
-                        _ => unreachable!(),
-                    },
-                    left: Box::new(left),
-                    right: Box::new(right),
-                });
-            },
-            _ => return Err(format!("unexpected character: {}", c)),
+            '&' => Ok(And),
+            '|' => Ok(Or),
+            '^' => Ok(Xor),
+            '=' => Ok(Leq),
+            '>' => Ok(Impl),
+            _ => Err(InvalidCharacter(c)),
         }
-    }
-    if stack.len() != 1 {
-        Err(format!("unbalanced expression: {}", input))
-    } else {
-        Ok(stack.pop().unwrap())
     }
 }
 
-impl std::fmt::Display for BinOp {
-    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+impl From<BinOp> for char {
+    fn from(op: BinOp) -> Self {
+        match op {
+            And => '&',
+            Or => '|',
+            Xor => '^',
+            Impl => '>',
+            Leq => '=',
+        }
+    }
+}
+
+impl fmt::Display for BinOp {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{}", char::from(*self))
+    }
+}
+
+impl fmt::Display for Node {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            And => write!(f, "&"),
-            Or => write!(f, "|"),
-            Xor => write!(f, "^"),
-            Impl => write!(f, ">"),
-            Leq => write!(f, "="),
+            Binary { op, left, right } => write!(f, "({} {} {})", left, op, right),
+            Not { operand } => write!(f, "!{}", operand),
+            Val(val) => write!(f, "{}", *val as u8),
         }
     }
 }
 
-fn eval_binary_ref(op: BinOp, left: &Node, right: &Node) -> bool {
-    let left = eval_tree_ref(left);
-    let right = eval_tree_ref(right);
-    match op {
-        And => left & right,
-        Or => left | right,
-        Xor => left ^ right,
-        Impl => !(left & !right),
-        Leq => left == right,
+impl fmt::Debug for ParseError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            MissingOperand => write!(f, "Missing operand"),
+            InvalidCharacter(c) => write!(f, "Invalid character: '{}'", c),
+            UnbalancedExpression => write!(f, "Unbalanced expression"),
+        }
     }
 }
 
-fn eval_tree_ref(node: &Node) -> bool {
-    match node {
-        Val(x) => *x,
-        Not { operand } => !eval_tree_ref(operand),
-        Binary { op, left, right } => eval_binary_ref(*op, left, right),
+impl std::str::FromStr for Node {
+    type Err = ParseError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let mut stack = Vec::with_capacity(42);
+        for c in s.chars() {
+            match c {
+                '0' => stack.push(Val(false)),
+                '1' => stack.push(Val(true)),
+                '!' => {
+                    let operand = stack.pop().ok_or(MissingOperand)?;
+                    stack.push(Not { operand: Box::new(operand) });
+                }
+                _ => {
+                    let op = c.try_into()?; // convert char to BinOp
+                    let right = stack.pop().ok_or(MissingOperand)?;
+                    let left = stack.pop().ok_or(MissingOperand)?;
+                    stack.push(Binary {
+                        op,
+                        left: Box::new(left),
+                        right: Box::new(right),
+                    });
+                }
+            }
+        }
+        if stack.len() != 1 {
+            Err(UnbalancedExpression)
+        } else {
+            Ok(stack.pop().unwrap())
+        }
     }
 }
 
-fn eval_binary(op: BinOp, left: Node, right: Node) -> bool {
-    let left = eval_tree(left);
-    let right = eval_tree(right);
-    match op {
-        And => left & right,
-        Or => left | right,
-        Xor => left ^ right,
-        Impl => !left | right,
-        Leq => left == right,
+impl From<&Box<Node>> for bool {
+    fn from(node: &Box<Node>) -> Self {
+        Self::from(node.as_ref())
     }
 }
 
-fn eval_tree(node: Node) -> bool {
-    match node {
-        Val(x) => x,
-        Not { operand } => !eval_tree(*operand),
-        Binary { op, left, right } => eval_binary(op, *left, *right),
+impl From<&Node> for bool {
+    fn from(node: &Node) -> Self {
+        match node {
+            Val(x) => *x,
+            Not { operand } => !Self::from(operand),
+            Binary { op, left, right } => {
+                let left = || Self::from(left);
+                let right = || Self::from(right);
+                match op {
+                    And =>  left() && right(),
+                    Or => left() || right(),
+                    Xor => left() ^ right(),
+                    Impl => !left() || right(),
+                    Leq => left() == right(),
+                }
+            },
+        }
+    }
+}
+
+impl From<Node> for bool {
+    fn from(node: Node) -> Self {
+        Self::from(&node)
     }
 }
 
@@ -127,10 +163,7 @@ fn print_dot_node(dot: &mut String, node: &Node) {
     let id = get_node_addr(node);
     match node {
         Val(v) => {
-            dot.push_str(&format!("{} [label=\"{}\"];\n", id, match v {
-                true => "1",
-                false => "0",
-            }));
+            dot.push_str(&format!("{} [label=\"{}\"];\n", id, *v as u8));
         },
         Binary { op, left, right } => {
             dot.push_str(&format!("{} [label=\"{}\"];\n", id, op));
@@ -148,34 +181,17 @@ fn print_dot_node(dot: &mut String, node: &Node) {
 }
 
 fn get_node_addr(node: &Node) -> usize {
-    use std::mem::transmute;
-    unsafe { transmute(node) }
+    unsafe { std::mem::transmute(node) }
 }
 
-/*
-fn print_node(node: Node) {
-    print!("(");
-    print_node(left);
-    print!("{}",
-        match op {
-            And => " && ",
-            Or => " || ",
-            Xor => " ^ ",
-            Impl => " -> ",
-            Leq => " <= ",
-        }
-}
-*/
-
-fn main() {
-    // lets try a long rpn expression
+fn main() -> Result<(), ParseError> {
     let input = random_rpn_expr();
-    let node = parse(&input).unwrap();
-    // call draw_ast on the root node
-    print_dot(&node);
-    // for debugging purposes, print the AST in stderr
+    eprintln!("Input:\n{}", input);
+    let node = input.parse()?;
     eprintln!("{}", input);
-    eprintln!("{}", eval_tree_ref(&node));
+    print_dot(&node);
+    eprintln!("{}", bool::from(&node));
+    Ok(())
 }
 
 use std::fs::File;
@@ -217,6 +233,76 @@ fn random_rpn_expr() -> String {
     rpn
 }
 
-#[test]
-fn tests() {
+// tests
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn to_bool(s: &str) -> bool {
+        s.parse::<Node>().unwrap().into()
+    }
+
+    #[test]
+    fn ex03_basic_tests() {
+        assert_eq!(to_bool("0"), false);
+        assert_eq!(to_bool("1"), true);
+        assert_eq!(to_bool("1!"), false);
+        assert_eq!(to_bool("0!"), true);
+        assert_eq!(to_bool("11&"), true);
+        assert_eq!(to_bool("10&"), false);
+        assert_eq!(to_bool("10|"), true);
+        assert_eq!(to_bool("01|"), true);
+        assert_eq!(to_bool("00|"), false);
+        assert_eq!(to_bool("10^"), true);
+        assert_eq!(to_bool("11^"), false);
+        assert_eq!(to_bool("10>"), false);
+        assert_eq!(to_bool("01>"), true);
+        assert_eq!(to_bool("10="), false);
+        assert_eq!(to_bool("11="), true);
+    }
+
+    #[test]
+    fn ex03_subject_tests() {
+        assert_eq!(to_bool("10&"), false);
+        assert_eq!(to_bool("10|"), true);
+        assert_eq!(to_bool("11>"), true);
+        assert_eq!(to_bool("10="), false);
+        assert_eq!(to_bool("1011||="), true);
+    }
+
+    #[test]
+    fn ex03_advanced_tests() {
+        assert_eq!(to_bool("1011||="), true);
+        assert_eq!(to_bool("111&!!!1|01=|=11>^0|0!1^11>1|0>1^>10^1|>10^>^"), true);
+    }
+
+    /*
+    #[test]
+    fn ex03_error_tests() {
+        unwrap_err!("1&".parse::<Node>());
+        unwrap_err!("1|".parse::<Node>());
+        unwrap_err!("1>".parse::<Node>());
+        unwrap_err!("1=".parse::<Node>());
+        unwrap_err!("1^".parse::<Node>());
+        unwrap_err!("1&1".parse::<Node>());
+        unwrap_err!("1|1".parse::<Node>());
+        unwrap_err!("1=1".parse::<Node>());
+        unwrap_err!("1^1".parse::<Node>());
+        unwrap_err!("1>1".parse::<Node>());
+
+        unwrap_err!("1x|".parse::<Node>());
+        unwrap_err!("1x&".parse::<Node>());
+        unwrap_err!("1x>".parse::<Node>());
+        unwrap_err!("1x=".parse::<Node>());
+        unwrap_err!("1x^".parse::<Node>());
+        unwrap_err!("1x!".parse::<Node>());
+
+        unwrap_err!("10&1".parse::<Node>());
+        unwrap_err!("10|1".parse::<Node>());
+        unwrap_err!("10>1".parse::<Node>());
+        unwrap_err!("10=1".parse::<Node>());
+        unwrap_err!("10^1".parse::<Node>());
+        unwrap_err!("10!".parse::<Node>());
+    }
+    */
 }
